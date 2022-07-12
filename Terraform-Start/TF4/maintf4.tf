acb43ed0610e61d2451fbed94fd34bbd7e7232b7
Mathis Hannes CKW <hannes.mathis@ckw.ch>
2022-07-12 17:01:11 +0200

    added values

--- Terraform-Start/TF4/maintf4.tf
+++ Terraform-Start/TF4/maintf4.tf
@@ -17,10 +17,10 @@ terraform {
 provider "azurerm" {
   features {}
   
-  subscription_id   = "<subscription_id>"
-  tenant_id         = "<tenant_id>"
-  client_id         = "<service_principal_appid>"
-  client_secret     = "<service_principal_password>"
+  subscription_id   = "af3ec114-8f3a-43b2-9bc7-8833c761d44b"
+  tenant_id         = "4abcb15f-9fd7-4a13-ac16-3d55a64fa21e>"
+  client_id         = "f792d999-fbdb-48a8-952b-86b5bebacff2"
+  client_secret     = "pHk8Q~nnyJ-FwQLlh9UGlemyabyl_lVYFEtJtda5"
 }
 
 
