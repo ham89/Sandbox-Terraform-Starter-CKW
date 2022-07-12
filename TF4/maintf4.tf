0bf004ce6e0cff4acc2927e85e34f9c9c642fe99
Mathis Hannes CKW <hannes.mathis@ckw.ch>
2022-07-12 17:06:08 +0200

    typo corrected

--- TF4/maintf4.tf
+++ TF4/maintf4.tf
@@ -10,20 +10,14 @@ terraform {
     }
   }
 }
-
-
-
-
 provider "azurerm" {
   features {}
   
   subscription_id   = "af3ec114-8f3a-43b2-9bc7-8833c761d44b"
-  tenant_id         = "4abcb15f-9fd7-4a13-ac16-3d55a64fa21e>"
+  tenant_id         = "4abcb15f-9fd7-4a13-ac16-3d55a64fa21e"
   client_id         = "f792d999-fbdb-48a8-952b-86b5bebacff2"
   client_secret     = "pHk8Q~nnyJ-FwQLlh9UGlemyabyl_lVYFEtJtda5"
 }
-
-
 
 
 resource "azurerm_resource_group" "rg" {
